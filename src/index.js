c62c41df09c35f08956143adec15d382f0c3f997
kashikaga <kgupta.abps@gmail.com>
2026-06-18 00:24:26 +0530

    added basic delete functionality

--- src/index.js
+++ src/index.js
@@ -15,8 +15,23 @@ function addTodo() {
 
     const li = document.createElement("li");
     li.textContent = nTask;
-
     li.classList.add("todo-item-box");
+
+    const del= document.createElement("button");
+    del.textContent="-";
+    del.classList.add("delete-btn");
+    
+    del.onclick= function(){
+        const index=todoList.indexOf(nTask);
+        if(index !== -1){
+            todoList.splice(index, 1);
+        }
+
+        li.remove();
+    };
+
+    li.appendChild(del);
+    
     ul.appendChild(li);
 
 
